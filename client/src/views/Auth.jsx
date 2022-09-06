c6a976408726624b0e0b2aa96171e946a5cedf2a
Ken Fukuyama <46236452+kenfukuyama@users.noreply.github.com>
2022-09-05 17:34:55 -0700

    implemented smooth client rendering

--- client/src/views/Auth.jsx
+++ client/src/views/Auth.jsx
@@ -1,146 +1,130 @@
-// import logo from './logo.svg';
+
+
 import {useState, useEffect} from 'react';
 import axios from 'axios';
 import jwt from 'jwt-decode';
-import {Routes, Route} from 'react-router-dom';
-// import { useNavigate } from 'react-router-dom';
-// import {Switch} from 'react-router';
+
 import Cookies from 'universal-cookie';
+import { useNavigate } from 'react-router-dom';
+import ReactLoading from 'react-loading';
 
 
-import './App.css';
-import Auth from './views/Auth';
-import Events from './views/Events';
-// import AuthenticatedRoute from './views/AuthenticatedRoute';
-// import UnauthenticatedRoute from './views/UnauthenticatedRoute';
-
-
-function App() {
-    const [authenticated, setAuthenticated] = useState(false);
-    const [loading, setLoading] = useState(true);
+function Auth (props) {
     const cookies = new Cookies();
-    // const navigate = useNavigate();
-    // const [user, setUser] = useState({
-    //     firstName: "nisrine",
-    //     lastName: "fukuyama",
-    //     email: "nisrine@gmail.com",
-    //     password: "mindlike",
-    //     confirmPassword: "mindlike"
-    // });  
+    const navigate = useNavigate();
 
-    // const [userLogIn, setUserLogIn] = useState({
-    //     email: "",
-    //     password: "",
-    // });
+    const [decided, setDecided] = useState(false);
 
-    // const createUser = (e) => {
-    //     e.preventDefault();
+    useEffect(() => {
+        if (props.authenticated) {
+            navigate('/events');
+        }
+        setDecided(true);
+    }, [props.loading])
+    
 
-    //     axios.post('http://localhost:8000/api/register', {
-    //     firstName: user.firstName,
-    //     lastName: user.lastName,
-    //     email: user.email,
-    //     password: user.password,
-    //     confirmPassword: user.confirmPassword
-    // }, {withCredentials: true} )
-    //     .then(res => {
-    //         const token = res.data.userToken
-    //         console.log(jwt(token));
-    //         console.log(res);
+    const [user, setUser] = useState({
+        firstName: "nisrine",
+        lastName: "fukuyama",
+        email: "nisrine@gmail.com",
+        password: "mindlike",
+        confirmPassword: "mindlike"
+    });  
 
-    //     })
-    //     .catch(err => console.error(err));
+    const [userLogIn, setUserLogIn] = useState({
+        email: "",
+        password: "",
+    });
+
+    const createUser = (e) => {
+        e.preventDefault();
+
+        axios.post('http://localhost:8000/api/register', {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        password: user.password,
+        confirmPassword: user.confirmPassword
+    }, {withCredentials: true} )
+        .then(res => {
+            const token = res.data.userToken;
+            console.log(jwt(token));
+            console.log(res);
+            props.setAuthenticated(true);
+            navigate('/events');
+
+        })
+        .catch(err => console.error(err));
 
         
-    // }
+    }
 
-    // const logInUser = (e) => {
-    //     e.preventDefault();
-    //     axios.post('http://localhost:8000/api/login', {
-    //         email: userLogIn.email,
-    //         password: userLogIn.password
-    //     }, {withCredentials: true})
-    //     .then(res => {
-    //         const token = res.data.userToken
-    //         console.log(jwt(token));
-    //         console.log(res);
+    const logInUser = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8000/api/login', {
+            email: userLogIn.email,
+            password: userLogIn.password
+        }, {withCredentials: true})
+        .then(res => {
+            const token = res.data.userToken
+            console.log(jwt(token));
+            console.log(res);
+            props.setAuthenticated(true);
+            // navigate('/events');
 
-    //     })
-    //     .catch(err => console.error(err));
-    // }
+        })
+        .catch(err => console.error(err));
+    }
 
-    // const logout = (e) => {
-    //     // e.preventDefault();
+    const logout = (e) => {
+        // e.preventDefault();
 
-    //     axios.get('http://localhost:8000/api/logout', {withCredentials: true} )
-    //     .then(res => { console.log(res);})
-    //     .catch(err =>{ console.error(err);});
-    // }
-
-
-    // const consoleLogCookie = (e) => {
-    //     const token = cookies.get('usertoken')
-    //     const userId = jwt(token).id;
-    //     console.log(jwt(token).id);
-
-    //     axios.get('http://localhost:8000/api/users/' + userId, {withCredentials: true})
-    //     .then(res => { console.log(res.data);})
-    //     .catch(err =>{ console.error(err);});
-    // }
-    
-    // const getUsers = (e) => {
-    //     e.preventDefault();
-
-    //     axios.get('http://localhost:8000/api/users', {withCredentials: true} )
-    //     .then(res => { console.log(res);})
-    //     .catch(err =>{ console.error(err);});
-    // }
+        axios.get('http://localhost:8000/api/logout', {withCredentials: true} )
+        .then(res => { 
+            console.log(res);
+            props.setAuthenticated(false);
+        })
+        .catch(err =>{ console.error(err);});
+    }
 
 
-    // const handleChange = (e) => {
-    //     setUser({...user, [e.target.name] : e.target.value});
-    // }
-
-    // const handleChangeLoggedIn = (e) => {
-    //     setUserLogIn({...userLogIn, [e.target.name] : e.target.value});
-
-    // }
-
-    const onLoad = () => {
+    const consoleLogCookie = (e) => {
         const token = cookies.get('usertoken')
-        console.log("running load: " + loading);
-
-        if (!token) {
-            setLoading(false);
-            return;
-        }
-
         const userId = jwt(token).id;
         console.log(jwt(token).id);
 
         axios.get('http://localhost:8000/api/users/' + userId, {withCredentials: true})
-        .then(res => { 
-            console.log(res.data); 
-            setAuthenticated(true);
-            setLoading(false);
-        })
-        .catch(err =>{ 
-            console.error(err);
-            setLoading(false);
-        });
+        .then(res => { console.log(res.data);})
+        .catch(err =>{ console.error(err);});
+    }
+    
+    const getUsers = (e) => {
+        e.preventDefault();
+
+        axios.get('http://localhost:8000/api/users', {withCredentials: true} )
+        .then(res => { console.log(res);})
+        .catch(err =>{ console.error(err);});
     }
 
 
+    const handleChange = (e) => {
+        setUser({...user, [e.target.name] : e.target.value});
+    }
 
-    // check whether users are logged in
-    useEffect( () => {
-        onLoad();
-    // eslint-disable-next-line
-    }, []);
+    const handleChangeLoggedIn = (e) => {
+        setUserLogIn({...userLogIn, [e.target.name] : e.target.value});
 
-    return (
-        <div className="App">
-                {/* <div className="container">
+    }
+
+    if (props.loading || !decided) {
+        console.log("loading bubles ... ");
+        return (<ReactLoading type='bubbles'/>);
+        // return <div>loading</div>;
+    }
+    else {
+        return (
+            <div>
+                <div className="container">
                     <div className="d-flex vh-100 align-items-center justify-content-center flex-column fade-in">
                         <div className="row">
                             <div className="col">
@@ -168,7 +152,7 @@ function App() {
                                                     onChange={handleChange}
                                                     className="form-control" />
                                             </div>
-
+    
                                             <div className="mb-2">
                                                 <label className="form-label">email</label><br />
                                                 <input
@@ -179,7 +163,7 @@ function App() {
                                                     onChange={handleChange}
                                                     className="form-control" />
                                             </div>
-
+    
                                             <div className="mb-2">
                                                 <label className="form-label">password</label><br />
                                                 <input
@@ -190,7 +174,7 @@ function App() {
                                                     onChange={handleChange}
                                                     className="form-control" />
                                             </div>
-
+    
                                             <div className="mb-2">
                                                 <label className="form-label">confirmPassword</label><br />
                                                 <input
@@ -201,29 +185,29 @@ function App() {
                                                     onChange={handleChange}
                                                     className="form-control" />
                                             </div>
-
-
-
+    
+    
+    
                                             <input type="submit" className="btn btn-primary" />
                                         </form>
-
+    
                                         <div className="d-flex gap-3 mt-3">
                                             <button className="btn btn-info" onClick={consoleLogCookie} >console log cookies</button>
                                             <button className="btn btn-danger" onClick={logout} >Logout</button>
                                             <button className="btn btn-warning" onClick={getUsers} >Get Users</button>
                                         </div>
-
+    
                                     </div>
                                 </div>
-
+    
                             </div>
-
+    
                             <div className="col">
                                 <div className="card bg-transparent">
                                     <h4 className="card-header p-4">Login</h4>
                                     <div className="card-body">
                                         <form onSubmit={logInUser}>
-
+    
                                             <div className="mb-2">
                                                 <label className="form-label">email</label><br />
                                                 <input
@@ -234,7 +218,7 @@ function App() {
                                                     onChange={handleChangeLoggedIn}
                                                     className="form-control" />
                                             </div>
-
+    
                                             <div className="mb-2">
                                                 <label className="form-label">password</label><br />
                                                 <input
@@ -245,49 +229,22 @@ function App() {
                                                     onChange={handleChangeLoggedIn}
                                                     className="form-control" />
                                             </div>
-
-                                            <input type="submit" className="btn btn-primary" value="Login"/>
+    
+                                            <input type="submit" className="btn btn-primary" value="Login" />
                                         </form>
-
+    
                                     </div>
                                 </div>
-
+    
                             </div>
                         </div>
-
+    
                     </div>
-                </div> */}
+                </div>
+            </div>
+        )
 
-
-            <Routes>
-                {/* // * main routes */}
-                <Route element={<Auth setAuthenticated = {setAuthenticated} authenticated = {authenticated} loading={loading}/>} path="/" />
-                <Route element={  <Events/>} path="/events" />
-                <Route element={<Auth setAuthenticated = {setAuthenticated} />} path="/login" />
-                {/* <Route element={<Events/>} path="/events" /> */}
-
-
-                    {/* <UnauthenticatedRoute
-                        path="/login"
-                        component={Auth}
-                        appProps={{ authenticated }}
-                    />
-                    <AuthenticatedRoute
-                        path="/events"
-                        component={Events}
-                        appProps={{ authenticated }}
-                    /> */}
-                    {/* <Route component={NotFound} /> */}
-            </Routes>
-
-
-
-
-
-        
-
-        </div>
-    );
+    }
 }
 
-export default App;
+export default Auth;
---
+++ client/src/views/Events.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useEffect } from 'react';
+// import { useNavigate } from 'react-router-dom';
+// import {useState} from 'react';
+// import Cookies from 'universal-cookie';
+// import axios from 'axios';
+// import jwt from 'jwt-decode';
+
+
+const Events = (props) => {
+    // const [user, setUser] = user();
+
+    useEffect(() => {
+        // if (!props.loggedIn) {
+        //     console.log(props.loggedIn);
+        // }
+
+    // eslint-disable-next-line
+    }, [])
+    
+
+  return (
+    <div>You successfully logged in
+        welcome!
+
+    </div>
+  )
+}
+
+export default Events;
